9bfe2c8fbf383fbf4639f5c99c33c38b3e32ab20
Simpson Du <simpson@spotpass.com>
2022-03-09 02:08:17 +0000

    Cancel own reservation

--- contracts/Booking.sol
+++ contracts/Booking.sol
@@ -6,38 +6,68 @@ contract Booking {
   uint8 private constant MAX_ROOMS = 20;
   uint8 private constant MAX_TIMESLOTS = 24;
 
+  enum BookingState {
+    None,
+    Reserved,
+    Cancelled
+  }
+
   Room[MAX_ROOMS] private rooms;
-  mapping(address => mapping(uint8 => uint8[])) users;
+  mapping(address => mapping(uint8 => BookingState[MAX_TIMESLOTS])) users;
 
   struct Room {
     address[MAX_TIMESLOTS] reservations;
   }
 
-  modifier roomNotAvailable(uint8 roomIdx, uint8 timeslotIdx) {
-    require(roomIdx < MAX_ROOMS ||
-      timeslotIdx < MAX_TIMESLOTS ||
+  modifier isRoomAvailable(uint8 roomIdx, uint8 timeslotIdx) {
+    require(roomIdx < MAX_ROOMS &&
+      timeslotIdx < MAX_TIMESLOTS &&
       rooms[roomIdx].reservations[timeslotIdx] == address(0), 
       "Room is not available");
     _;
   }
 
-  event RoomReserved(uint8 indexed roomIdx, uint8 indexed timeslotIdx, address user);
-  event UserUpdated(uint8 indexed roomIdx, uint8 indexed timeslotIdx, address user);
+  modifier onlyOwner(uint8 roomIdx, uint8 timeslotIdx){
+    require(msg.sender == rooms[roomIdx].reservations[timeslotIdx], 
+      "You are not the room owner");
+    _;
+  }
+
+  event RoomReserved(uint8 indexed roomIdx, 
+    uint8 indexed timeslotIdx, 
+    address user);
+  event RoomCancelled(uint8 indexed roomIdx, 
+    uint8 indexed timeslotIdx, 
+    address user);
+  event UserUpdated(address indexed user, 
+    uint8 indexed roomIdx, 
+    uint8 indexed timeslotIdx,
+    BookingState bookingState);
 
   function listRooms() external view returns (Room[20] memory) {
     return rooms;
   }
 
-  function getMyBooking(uint8 roomIdx) external view returns (uint8[] memory) {
+  function getMyBooking(uint8 roomIdx) external view 
+    returns (BookingState[MAX_TIMESLOTS] memory) {
     return users[msg.sender][roomIdx];
   }
 
-  function reserve(uint8 roomIdx, uint8 timeslotIdx) public
-    roomNotAvailable(roomIdx, timeslotIdx) {
+  function reserve(uint8 roomIdx, uint8 timeslotIdx) external
+    isRoomAvailable(roomIdx, timeslotIdx) {
       rooms[roomIdx].reservations[timeslotIdx] = msg.sender;
       emit RoomReserved(roomIdx, timeslotIdx, msg.sender);
 
-      users[msg.sender][roomIdx].push(timeslotIdx);
-      emit UserUpdated(roomIdx, timeslotIdx, msg.sender);
+      users[msg.sender][roomIdx][timeslotIdx] = BookingState.Reserved;
+      emit UserUpdated(msg.sender, roomIdx, timeslotIdx, BookingState.Reserved);
   }
+
+  function cancel(uint8 roomIdx, uint8 timeslotIdx) external 
+    onlyOwner(roomIdx, timeslotIdx) {
+      rooms[roomIdx].reservations[timeslotIdx] = address(0);
+      emit RoomCancelled(roomIdx, timeslotIdx, msg.sender);
+
+      users[msg.sender][roomIdx][timeslotIdx] = BookingState.Cancelled;
+      emit UserUpdated(msg.sender, roomIdx, timeslotIdx, BookingState.Cancelled);
+  } 
 }
--- test/booking-test.js
+++ test/booking-test.js
@@ -10,17 +10,47 @@ describe("Booking", function () {
     await booking.deployed();
   });
 
-  it("Should return the list of rooms", async function () {
+  it("Should be able to return the list of rooms", async function () {
     const rooms = await booking.listRooms();
     expect(rooms.length).to.equal(20);
   });
 
-  it("Should reserve room if it is available", async function () {
+  it("Should be able to reserve room if it is available", async function () {
     const roomIdx = 1;
     const timeslotIdx = 8;
 
     await booking.reserve(roomIdx, timeslotIdx);
     const myBooking = await booking.getMyBooking(roomIdx);
-    expect(myBooking).contains(timeslotIdx);
+    expect(myBooking[timeslotIdx]).to.equal(1);
+  });
+
+  it("Should revert if the room is not available", async function () {
+    const roomIdx = 1;
+    const timeslotIdx = 8;
+
+    await booking.reserve(roomIdx, timeslotIdx);
+    await expect (
+      booking.reserve(roomIdx, timeslotIdx)
+    ).to.be.revertedWith("Room is not available");
+  });
+
+  it("Should not cancel the room if you are not the owner", async function () {
+    const roomIdx = 2;
+    const timeslotIdx = 10;
+
+    await expect(booking.cancel(roomIdx, timeslotIdx)).to.be.revertedWith(
+      "You are not the room owner"
+    );
+  });
+
+  it("Should be able to cancel the room if you are the owner", async function () {
+    const roomIdx = 2;
+    const timeslotIdx = 10;
+
+    await booking.reserve(roomIdx, timeslotIdx);
+    await booking.cancel(roomIdx, timeslotIdx);
+
+    const myBooking = await booking.getMyBooking(roomIdx);
+    expect(myBooking[timeslotIdx]).to.equal(2);
   });
 });
