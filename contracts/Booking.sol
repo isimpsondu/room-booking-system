25478d98b9f84d4614a8ad33d890a4aadc0dfe88
Simpson Du <simpson@spotpass.com>
2022-03-09 01:15:17 +0000

    Reserve room

--- contracts/Booking.sol
+++ contracts/Booking.sol
@@ -3,14 +3,41 @@ pragma solidity ^0.8.0;
 
 contract Booking {
 
-  uint64[20] private roomIds;
-  mapping(uint64 => Room) private rooms;
+  uint8 private constant MAX_ROOMS = 20;
+  uint8 private constant MAX_TIMESLOTS = 24;
+
+  Room[MAX_ROOMS] private rooms;
+  mapping(address => mapping(uint8 => uint8[])) users;
 
   struct Room {
-    mapping (uint64 => address) reservations;
+    address[MAX_TIMESLOTS] reservations;
   }
 
-  function listRooms() external view returns (uint64[20] memory) {
-    return roomIds;
+  modifier roomNotAvailable(uint8 roomIdx, uint8 timeslotIdx) {
+    require(roomIdx < MAX_ROOMS ||
+      timeslotIdx < MAX_TIMESLOTS ||
+      rooms[roomIdx].reservations[timeslotIdx] == address(0), 
+      "Room is not available");
+    _;
+  }
+
+  event RoomReserved(uint8 indexed roomIdx, uint8 indexed timeslotIdx, address user);
+  event UserUpdated(uint8 indexed roomIdx, uint8 indexed timeslotIdx, address user);
+
+  function listRooms() external view returns (Room[20] memory) {
+    return rooms;
+  }
+
+  function getMyBooking(uint8 roomIdx) external view returns (uint8[] memory) {
+    return users[msg.sender][roomIdx];
+  }
+
+  function reserve(uint8 roomIdx, uint8 timeslotIdx) public
+    roomNotAvailable(roomIdx, timeslotIdx) {
+      rooms[roomIdx].reservations[timeslotIdx] = msg.sender;
+      emit RoomReserved(roomIdx, timeslotIdx, msg.sender);
+
+      users[msg.sender][roomIdx].push(timeslotIdx);
+      emit UserUpdated(roomIdx, timeslotIdx, msg.sender);
   }
 }
--- test/booking-test.js
+++ test/booking-test.js
@@ -2,12 +2,25 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Booking", function () {
-  it("Should return the list of rooms", async function () {
-    const Booking = await ethers.getContractFactory("Booking");
-    const booking = await Booking.deploy();
-    await booking.deployed();
+  let booking;
 
+  beforeEach(async () => {
+    const Booking = await ethers.getContractFactory("Booking");
+    booking = await Booking.deploy();
+    await booking.deployed();
+  });
+
+  it("Should return the list of rooms", async function () {
     const rooms = await booking.listRooms();
     expect(rooms.length).to.equal(20);
+  });
+
+  it("Should reserve room if it is available", async function () {
+    const roomIdx = 1;
+    const timeslotIdx = 8;
+
+    await booking.reserve(roomIdx, timeslotIdx);
+    const myBooking = await booking.getMyBooking(roomIdx);
+    expect(myBooking).contains(timeslotIdx);
   });
 });
